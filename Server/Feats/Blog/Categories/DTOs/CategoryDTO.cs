54240ce01a62f2058393753d7b122419c208dc00
Wilson Oquendo <oquendo999@hotmail.com>
2024-02-12 19:15:52 -0500

    feat: created tests for creating categories

--- Server/Feats/Blog/Categories/DTOs/CategoryDTO.cs
+++ Server/Feats/Blog/Categories/DTOs/CategoryDTO.cs
@@ -1,11 +1,21 @@
 ﻿using Shared.Models.Blog;
+using System.ComponentModel.DataAnnotations;
 
 namespace Server.Feats.Blog.Categories.DTOs;
 
 public class CategoryDTO
 {
     public Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(256)]
     public string Name { get; set; }
+
+    [Required]
+    [MaxLength(128)]
     public string ThumbnailImage { get; set; }
+
+    [Required]
+    [MaxLength(1024)]
     public string Description { get; set; }
 }
---
+++ Tests/ServerTests/Blog/Categories/Controllers/CategoryControllerTests.cs
@@ -0,0 +1,93 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Server.Data;
+using Server.Feats.Blog.Categories.Commands;
+using Server.Feats.Blog.Categories.Controllers;
+using Server.Feats.Blog.Categories.DTOs;
+using System.Net;
+using Tests.ServerTests.Helpers;
+
+namespace Tests.ServerTests.Blog.Categories.Controllers;
+public class CategoryControllerTests
+{
+    private readonly CategoriesController categoriesController;
+    private readonly Mock<IMediator> mediatorMoq;
+    private readonly Mock<IWebHostEnvironment> hostEnvironmentMoq;
+    private readonly DbContextOptions<AppDbContext> options;
+    public readonly AppDbContext contextFake;
+    private readonly Fixture fixture;
+
+    public CategoryControllerTests()
+    {
+        fixture = new();
+        mediatorMoq = new();
+        hostEnvironmentMoq = new();
+        options = HelperMethods.GenerateOptions();
+        contextFake = new(options);
+        categoriesController = new(mediatorMoq.Object, hostEnvironmentMoq.Object);
+    }
+
+    [Fact]
+    public async Task Controller_ModelState_ShouldReturnBadRequest()
+    {
+        CategoryDTO? category = new()
+        {
+            Name = String.Empty,
+            ThumbnailImage = String.Empty,
+        };
+
+        categoriesController.ModelState
+            .AddModelError("Name", "Required");
+        categoriesController.ModelState
+            .AddModelError("ThumbnailImage", "Required");
+
+        IActionResult? result = await categoriesController.Create(category);
+
+        result.Should().BeOfType<BadRequestObjectResult>();
+    }
+
+    [Fact]
+    public async Task Controller_ShouldNotBeNull()
+    {
+        CategoryDTO? category = null;
+
+        IActionResult? result = await categoriesController.Create(category);
+
+        result.Should().BeOfType<BadRequestObjectResult>();
+    }
+
+    [Fact]
+    public async Task Controller_ModelState_ShouldReturn500Error()
+    {
+        CategoryDTO? category = new();
+        categoriesController.StatusCode(500);
+
+        mediatorMoq.Setup(mediator => mediator.Send(It.IsAny<CreateCategoryCommandRequest>(), default))
+                  .ReturnsAsync(0);
+
+
+        IActionResult result = await categoriesController.Create(category)!;
+        ObjectResult? objectResult = (result as ObjectResult)!;
+        objectResult.StatusCode.Should().Be((int)HttpStatusCode.InternalServerError);
+    }
+
+    [Fact]
+    public async Task Controller_ShouldReturnCreated()
+    {
+        CategoryDTO? category = fixture.Create<CategoryDTO>();
+        categoriesController.StatusCode(201);
+
+        mediatorMoq.Setup(mediator => mediator.Send(It.IsAny<CreateCategoryCommandRequest>(), default))
+                  .ReturnsAsync(1);
+
+
+        IActionResult result = await categoriesController.Create(category)!;
+        ObjectResult? objectResult = (result as ObjectResult)!;
+        objectResult.StatusCode.Should().Be((int)HttpStatusCode.Created);
+    }
+}
